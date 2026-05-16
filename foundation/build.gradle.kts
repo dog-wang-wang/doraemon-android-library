238ee61eac9cf0505b2554852d965cc149e07c80
zjh <1123019893@qq.com>
2026-05-16 21:23:30 +0800

    feat: 更正foundation模块的gradle

--- foundation/build.gradle.kts
+++ foundation/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -20,11 +19,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }
-    buildFeatures {
-        viewBinding = true
-        dataBinding = true
-        compose = true
     }
 }
 
