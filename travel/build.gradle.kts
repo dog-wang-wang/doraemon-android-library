d30da5a6b1f88dbf411de097915cf7ee45667179
zjh <1123019893@qq.com>
2026-05-30 15:33:22 +0800

    feat: travel依赖修改

--- travel/build.gradle.kts
+++ travel/build.gradle.kts
@@ -37,13 +37,8 @@ android {
 }
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
-    implementation(libs.lifecycle.runtime.ktx)
+    implementation(project(":foundation"))
+    implementation(project(":foundation-ui-compose"))
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
